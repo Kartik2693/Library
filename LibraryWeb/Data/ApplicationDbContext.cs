5409d5fcee9910ad5f92e404f4b9d35e77ed1e86
Kartik Sherigar <sherigarkartik786@gmail.com>
2023-11-08 13:46:35 +0530

    Add project files.

---
+++ LibraryWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using LibraryWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+                    
+        }
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, CategoryName = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, CategoryName = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, CategoryName = "History", DisplayOrder = 3 }
+                );
+        }
+    }
+}
