1662a17602020ad09c8af7627af79e22383ea493
Kartik Sherigar <sherigarkartik786@gmail.com>
2023-11-11 19:31:06 +0530

    New One

---
+++ Library.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Library.Models;
+
+namespace Library.DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+    {
+                
+    }
+    public DbSet<Category> Categories { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>().HasData(
+            new Category { Id = 1, CategoryName = "Action", DisplayOrder = 1 },
+            new Category { Id = 2, CategoryName = "SciFi", DisplayOrder = 2 },
+            new Category { Id = 3, CategoryName = "History", DisplayOrder = 3 }
+            );
+    }
+}
+}
--- Library.Models/Category.cs
+++ Library.Models/Category.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
-namespace LibraryWeb.Models
+namespace Library.Models
 {
     public class Category
     {
@@ -13,7 +13,7 @@ namespace LibraryWeb.Models
         public string CategoryName { get; set; }
 
         [DisplayName("Display Order")]
-        [Range(1,100)]
+        [Range(1, 100)]
         public int DisplayOrder { get; set; }
     }
 }
